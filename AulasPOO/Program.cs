2330cddff5af733a16822c3fca9435b2e12f261c
RodegheriLucas <orodegherilucas@gmail.com>
2024-08-13 20:48:52 -0300

    Multiplicação e Adição POO

---
+++ AulasPOO/Program.cs
@@ -0,0 +1,36 @@
+﻿using AulasPOO.uteis;
+int opcao = 0, num1, num2;
+do
+{
+    Console.WriteLine("Escolha uma opção: ");
+    Console.WriteLine("1 - Somar\n2 - Multiplicar\n0 - Sair");
+    opcao = Convert.ToInt32(Console.ReadLine());
+    switch (opcao)
+    {
+        case 0:
+            Console.WriteLine("Você escolheu sair.");
+            break;
+        case 1:
+            LerDoisNumerosinteiros();
+            var calc = new Calculadora();
+            int resultado = calc.Soma(num1, num2);
+            Console.WriteLine($"O resultado é {resultado}");
+            break;
+        case 2:
+            LerDoisNumerosinteiros();
+            Console.WriteLine($"O resultado é {Calculadora.Mult(num1, num2)}");
+            break;
+        default:
+            Console.WriteLine("Opção invalida.");
+            break;
+    }
+} while (opcao != 0);
+
+void LerDoisNumerosinteiros()
+{
+    Console.WriteLine("Digite o primeiro numero: ");
+    num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o segundo numero: ");
+    num2 = int.Parse(Console.ReadLine()); ;
+
+}
